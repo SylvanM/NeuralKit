34d613afae9cdd92858a727b7e99866d687b0919
Sylvan Martin <alephnulltim@gmail.com>
2022-07-10 19:35:50 -0400

    Added DataSet tests

---
+++ Tests/NeuralKitTests/Examples/GeneticDigits.swift
@@ -0,0 +1,17 @@
+//
+//  GeneticDigits.swift
+//  
+//
+//  Created by Sylvan Martin on 7/5/22.
+//
+
+import XCTest
+
+/**
+ * A test suite demonstrating training a neural network to recognize "handwritten" digits using a genetic algorithm
+ */
+class GeneticDigits: XCTestCase {
+
+    
+
+}
