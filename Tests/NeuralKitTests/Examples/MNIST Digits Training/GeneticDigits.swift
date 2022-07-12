183c3c02db45c1c4c384098388cc82631ba11747
Sylvan Martin <alephnulltim@gmail.com>
2022-07-12 01:01:32 -0400

    Added test for genetic digits recognition

--- Tests/NeuralKitTests/Examples/MNIST Digits Training/GeneticDigits.swift
+++ Tests/NeuralKitTests/Examples/MNIST Digits Training/GeneticDigits.swift
@@ -6,12 +6,55 @@
 //
 
 import XCTest
+import NeuralKit
 
 /**
  * A test suite demonstrating training a neural network to recognize "handwritten" digits using a genetic algorithm
  */
 class GeneticDigits: XCTestCase {
-
     
+    let digitsDataSet = try! DataSet(name: "Digits", inDirectory: URL(fileURLWithPath: "/Users/sylvanm/Programming/Machine Learning/Data sets/NKDS Sets/MNIST Digits"))
+    let nnShape = [784, 16, 16, 10]
+
+    func testGeneticDigits() throws {
+        let optimizer = GeneticOptimizer(optimizingShape: nnShape, activationFunction: .sigmoid, withData: digitsDataSet, breedingMethod: .arithmeticMean())
+        
+        let populationSize = 1000
+        let cutoff = 0.5
+        let iterations = 5000
+        
+        let recognizer = optimizer.findOptimalNetwork(populationSize: populationSize, eliminatingPortion: cutoff, iterations: iterations)
+        
+        let classifier = Classifier(fromNetwork: recognizer) { vector -> Int in
+            var digit = 0
+            
+            for i in 0..<vector.flatmap.count {
+                if vector.flatmap[i] > vector.flatmap[digit] {
+                    digit = i
+                }
+            }
+            
+            return digit
+        }
+        
+        var counter = 0
+        digitsDataSet.iterateTestingData { testingItem in
+            
+            if counter % 1000 == 0 {
+                
+                print("-------------------------")
+                
+                let mnistItem = MNISTUtility.MNISTItem(input: testingItem.input, output: testingItem.output)
+                
+                let decision = classifier.classify(testingItem.input)
+                
+                print(mnistItem)
+                print("Classifier says: \(decision)")
+                
+            }
+            
+            counter += 1
+        }
+    }
 
 }
