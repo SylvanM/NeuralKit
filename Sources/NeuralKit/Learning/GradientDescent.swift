f41755ae855de808646e200cf0fad5c3ee369d24
Sylvan Martin <alephnulltim@gmail.com>
2022-07-13 14:08:42 -0400

    Clarified and fixed the off by one error stuff

--- Sources/NeuralKit/Learning/GradientDescent.swift
+++ Sources/NeuralKit/Learning/GradientDescent.swift
@@ -14,20 +14,31 @@ import MatrixKit
 public class GradientDescent {
     
     /**
-     * Returns the gradient, in weight space, of the cost function for a particular training example.
+     * Computes the gradients, in weight space, of the cost function for a particular training example.
+     *
+     * This is just backpropogation for the weights.
      *
      * - Parameter network: The network to compute the cost of
      * - Parameter example: The training example to compute the gradient for
+     * - Parameter gradients: The array of weight gradients to be "filled" by this function
+     *
+     * - Precondition: `gradients.count == network.weights.count`
      */
-    public static func computeWeightGradient(of network: NeuralNetwork, forExample example: DataSet.Item) -> [Matrix] {
+    public static func computeWeightGradients(ofNetwork network: NeuralNetwork, forExample example: DataSet.Item, gradients: inout [Matrix]) {
+        
+        // all the comments here are to keep things straight in my mind, because there's a lot of
+        // room to make an off-by-one error here.
         
         // just initialize all these to empty arrays, this only works because Matrix is a value type and MUST STAY THAT WAY PLEASE
-        var activations = [Matrix](repeating: Matrix(), count: network.weights.count + 1)
+        var activations = [Matrix](repeating: Matrix(), count: network.layerCount)
     
-        var derivatives = [Matrix](repeating: Matrix(), count: network.weights.count)
-        var partials = activations
+        // these are the computed derivatives for each layer EXCEPT the first. Therefore,
+        // derivatives[i] refers to the derivative vector for the (i + 1)-th layer
+        var derivatives = [Matrix](repeating: Matrix(), count: network.layerCount - 1)
         
-        var gradients = network.weights
+        // these partial activations are the input to the activation function for each layer EXCEPT the first one,
+        // like the derivatives array.
+        var partials = derivatives
         
         network.feedForward(input: example.input, cache: &activations, beforeAdjustedCache: &derivatives)
         
@@ -39,20 +50,20 @@ public class GradientDescent {
             costGradient.flatmap[i] *= 2
         }
         
+        // compute all the derivatives at the pre-activated neuron value (before act. func. applied)
         for i in 0..<derivatives.count {
             derivatives[i].applyToAll(network.activationFunction.applyDerivative)
         }
         
         // compute the partials and gradient for the last layer so the rest are easy
-        partials[partials.count - 1]    = derivatives[partials.count - 1].hadamard(with: costGradient)
-        gradients[gradients.count - 1]  = activations[partials.count - 2].leftMultiply(by: partials[partials.count])
+        partials[partials.count - 1]    = derivatives[derivatives.count - 1].hadamard(with: costGradient)
+        gradients[gradients.count - 1]  = activations[partials.count - 2].leftMultiply(by: partials[partials.count - 1])
+        
         
         for i in (0..<(partials.count - 1)).reversed() {
             partials[i]  = derivatives[i].hadamard(with: network.weights[i]) // might need to index weights 1 higher
             gradients[i] = activations[i].transpose.leftMultiply(by: partials[i])
         }
-        
-        return gradients
         
     }
     
